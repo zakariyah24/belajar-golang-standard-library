cdc22680f975f0fab19f65b32db19d15de22a71a
zaka <fzakariyah1@gmail.com>
2024-03-26 22:13:26 +0700

    base 64

---
+++ base64.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+	"encoding/base64"
+	"fmt"
+)
+
+func main() {
+	value := "Zakariyah Firmansyah"
+
+	encoded := base64.StdEncoding.EncodeToString([]byte(value))
+
+	fmt.Println(encoded)
+
+	decoded, err := base64.StdEncoding.DecodeString(encoded)
+	if err != nil {
+		fmt.Println("error", err.Error())
+	} else {
+		fmt.Println(string(decoded))
+	}
+}
