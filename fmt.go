ddde18eafab63922260eec17cc29cc0928625728
zaka <fzakariyah1@gmail.com>
2024-03-31 22:42:46 +0700

    fmt

---
+++ fmt.go
@@ -0,0 +1,12 @@
+package main
+
+import "fmt"
+
+func main() {
+	firstName := "Zakariyah"
+	lastName := "Firmansyah"
+
+	fmt.Println("Hello '", firstName, lastName, "'")
+
+	fmt.Printf("Hello '%s %s'\n", firstName, lastName)
+}
