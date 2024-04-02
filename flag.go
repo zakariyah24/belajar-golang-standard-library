9ca56f70f793c73c9edc932a2148070377fe1455
zaka <fzakariyah1@gmail.com>
2024-04-02 20:53:36 +0700

    flag

---
+++ flag.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+)
+
+func main() {
+	var username *string = flag.String("username", "root", "database username")
+	var password *string = flag.String("password", "root", "database password")
+	var host *string = flag.String("host", "localhost", "database password")
+	var port *int = flag.Int("port", 0, "database password")
+
+	flag.Parse()
+
+	fmt.Println("username", *username)
+	fmt.Println("password", *password)
+	fmt.Println("host", *host)
+	fmt.Println("port", *port)
+
+}
