f838241cad75d5d8d2ce1aa1c6ae7a3ee648e2c0
zaka <fzakariyah1@gmail.com>
2024-04-02 20:53:07 +0700

    os

---
+++ os.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+	"fmt"
+	"os"
+)
+
+func main() {
+	args := os.Args
+	for _, arg := range args {
+		fmt.Println(arg)
+	}
+
+	hostname, err := os.Hostname()
+	if err == nil {
+		fmt.Println(hostname)
+	} else {
+		fmt.Println("error", err.Error())
+	}
+
+}
