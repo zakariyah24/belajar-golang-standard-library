6426ef0217f731165fa2ded7aaf4ea409a0b7f79
zaka <fzakariyah1@gmail.com>
2024-04-07 22:20:05 +0700

    errors

---
+++ errors.go
@@ -0,0 +1,36 @@
+package main
+
+import (
+	"errors"
+	"fmt"
+)
+
+var (
+	ValidationError = errors.New("Validation error")
+	NotFoundError   = errors.New("Not found error")
+)
+
+func GetById(id string) error {
+	if id == "" {
+		return ValidationError
+	}
+
+	if id != "eko" {
+		return NotFoundError
+	}
+	return nil
+
+}
+
+func main() {
+	err := GetById("eko")
+	if err != nil {
+		if errors.Is(err, ValidationError) {
+			fmt.Println("validation error")
+		} else if errors.Is(err, NotFoundError) {
+			fmt.Println("not found error")
+		} else {
+			fmt.Println("unknown error")
+		}
+	}
+}
