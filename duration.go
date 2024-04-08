514c2fc78c9d1ca661d3c2c470346d85ef116e89
zaka <fzakariyah1@gmail.com>
2024-04-08 23:15:48 +0700

    duration

---
+++ duration.go
@@ -0,0 +1,14 @@
+package main
+
+import (
+	"fmt"
+	"time"
+)
+
+func main() {
+	var duration1 time.Duration = time.Second * 100
+	var duration2 time.Duration = time.Millisecond * 10
+	var duration3 time.Duration = duration1 - duration2
+	fmt.Println(duration3)
+	fmt.Printf("duration : %d\n", duration3)
+}
